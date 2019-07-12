39e611292c5b43f06844a4aa66f3ef7044183ca9
Scott Steele <scottlsteele@gmail.com>
2019-07-11 20:29:50 -0400

    squash! rename output => expected_output

--- src/main.rs
+++ src/main.rs
@@ -263,25 +263,31 @@ fn fish_options(options: &str, existing_options: &mut HashSet<String>) -> Vec<St
 
 #[test]
 fn test_fish_options() {
-    let output: Vec<String> = vec!["-s 'f'".into(), "-l 'force'".into()];
+    let expected_output: Vec<String> = vec!["-s 'f'".into(), "-l 'force'".into()];
     let options = "-f, --force[=false]";
     let mut existing_options: HashSet<String> = Default::default();
-    assert_eq!(fish_options(options, &mut existing_options), output);
     assert_eq!(
-        output.iter().cloned().collect::<HashSet<_>>(),
+        fish_options(options, &mut existing_options),
+        expected_output
+    );
+    assert_eq!(
+        expected_output.iter().cloned().collect::<HashSet<_>>(),
         existing_options
     );
 
     use std::iter::once;
 
-    let output: Vec<String> = vec!["-l 'force'".into()];
+    let expected_output: Vec<String> = vec!["-l 'force'".into()];
     let options = "-f, --force[=false]";
     let mut existing_options: HashSet<String> = Default::default();
     existing_options.insert("-s 'f'".into());
     existing_options.insert("-l 'something'".into());
-    assert_eq!(fish_options(options, &mut existing_options), output);
     assert_eq!(
-        output
+        fish_options(options, &mut existing_options),
+        expected_output
+    );
+    assert_eq!(
+        expected_output
             .iter()
             .cloned()
             .chain(once("-s 'f'".to_string()))
