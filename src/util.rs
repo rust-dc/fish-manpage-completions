19e8232c0acc9fd8cea27cca1a8e31fcf488ac3f
Charlie Thomson <charliecthomson@gmail.com>
2019-11-07 18:53:24 -0500

    Added `util:translate` and `util:maketrans`
    These functions mirror their python counterparts, `translate` takes a String and a table (`HashMap<char, char>`) and remaps the String based on the table; and `maketrans` takes two equal length Strings and creates a table for use in the translate function.

--- src/util.rs
+++ src/util.rs
@@ -7,3 +7,64 @@ macro_rules! regex {
         &REGEX
     }};
 }
+
+use std::{
+    iter::FromIterator,
+    collections::HashMap,
+};
+
+
+/// Helper for `translate`, does the same thing as string.maketrans in python
+/// requires that f and t are the same length, or it takes the shorter one
+pub fn maketrans(f: String, t: String) -> HashMap<u8, char> {
+    HashMap::from_iter(f.bytes().zip(t.chars()))
+}
+
+/// Does effectively the same as string.translate in python, in 
+/// conjunction with `maketrans`, basically just replaces
+pub fn translate(s: String, table: HashMap<u8, char>) -> String {
+    let mut output = String::new();
+    for byte in s.bytes() {
+        if let Some(c) = table.get(&byte) {
+            output.push(*c);
+        } else {
+            output.push(byte as char);
+        }
+    }
+
+    output
+}
+
+#[test]
+fn test_maketrans() {
+    let mut expected = HashMap::new();
+    expected.insert(0x61, 'd');
+    expected.insert(0x62, 'e');
+    expected.insert(0x63, 'f');
+    assert_eq!(
+        maketrans(
+            "abc".to_owned(),
+            "def".to_owned(),
+        ),
+        expected
+    );
+}
+
+#[test]
+fn test_translate() {
+    // >>> "Hello World!".translate(str.maketrans("HlW", "aBc"))
+    // 'aeBBo corBd!'
+
+    let expected = "aeBBo corBd!".to_owned();
+
+    assert_eq!(
+        translate(
+            "Hello World!".to_owned(),
+            maketrans(
+                "HlW".to_owned(),
+                "aBc".to_owned(),
+            )
+        ),
+        expected,
+    )
+}
