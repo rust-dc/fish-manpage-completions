d100f86ca11d8b1f7a1e4113ad7ada90b0caf8e1
Ivan Tham <pickfire@riseup.net>
2020-01-11 09:38:26 +0800

    Use contains
    Co-Authored-By: Charlie Thomson <charliecthomson@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -564,7 +564,7 @@ impl ManParser for Type1 {
             let data: Vec<_> = data.split(".RS 4").collect();
             if data.len() > 1 {
                 let option_name = data[0].trim();
-                if option_name.find('-').is_some() {
+                if option_name.contains('-') {
                     let option_name = unquote_double_quotes(option_name);
                     let option_name = unquote_single_quotes(option_name);
                     let option_desc = data[1].trim().replace('\n', " ");
